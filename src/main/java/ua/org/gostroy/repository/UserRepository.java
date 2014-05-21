59f82ef70496ee9755985b94f2a62b79430ae866
panser <zzzibiaz>
2014-05-21 18:59:56 +0300

    fix problem

--- src/main/java/ua/org/gostroy/repository/UserRepository.java
+++ src/main/java/ua/org/gostroy/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package ua.org.gostroy.repositories;
+package ua.org.gostroy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
